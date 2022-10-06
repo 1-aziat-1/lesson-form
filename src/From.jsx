07ea86937448a1a384fda32614ae06ae08fbcfeb
1-aziat-1 <as.ac2727@mail.ru>
2022-10-07 02:20:08 +0300

    verstka

--- src/From.jsx
+++ src/From.jsx
@@ -1,7 +1,25 @@
+import _ from './Form.module.css';
 
 export const Form = () => {
   
   return (
-    <form></form>
+    <form className={_.form}>
+      <div className={_.wrap}>
+        <label className={_.label} htmlFor='email'>Email</label>
+        <input className={_.input} type='text' id='email' name='email'/>
+        <p className={_.error}>Сообщение об ошибке</p>
+      </div>
+      <div className={_.wrap}>
+        <label className={_.label} htmlFor='password'>Пароль</label>
+        <input className={_.input} type='password' id='password' name='password'/>
+        <p className={_.error}>Сообщение об ошибке</p>
+      </div>
+      <div className={_.wrapCheckbox}>
+        <input className={_.checkbox} type='checkbox' id='save' name='save'/>
+        <label className={_.label} htmlFor='save'>Сохранить Пароль</label>
+      </div>
+
+      <button className={_.submit} type='submit'>Войти</button>
+    </form>
   );
 };
