b80aa0cb20b1a7f4288f1dddd7600f068fe375d7
1-aziat-1 <as.ac2727@mail.ru>
2022-10-08 00:16:45 +0300

    ready html forms

--- src/From.jsx
+++ src/From.jsx
@@ -4,19 +4,22 @@ export const Form = () => {
   
   return (
     <form className={_.form}>
+
       <div className={_.wrap}>
         <label className={_.label} htmlFor='email'>Email</label>
         <input className={_.input} type='text' id='email' name='email'/>
         <p className={_.error}>Сообщение об ошибке</p>
       </div>
+
       <div className={_.wrap}>
         <label className={_.label} htmlFor='password'>Пароль</label>
         <input className={_.input} type='password' id='password' name='password'/>
         <p className={_.error}>Сообщение об ошибке</p>
       </div>
+
       <div className={_.wrapCheckbox}>
         <input className={_.checkbox} type='checkbox' id='save' name='save'/>
-        <label className={_.label} htmlFor='save'>Сохранить Пароль</label>
+        <label className={_.labelCheckbox} htmlFor='save'>Сохранить пароль</label>
       </div>
 
       <button className={_.submit} type='submit'>Войти</button>
